aeac06d7791c4e2d2cf0662a96182b01d721c304
Norcross <andrew.norcross@gmail.com>
2026-09-14 16:16:25 -0400

    Update bulk-actions.php

--- includes/admin/bulk-actions.php
+++ includes/admin/bulk-actions.php
@@ -14,30 +14,30 @@ use Norcross\ContentColdStorage\Admin\Config as AdminConfig;
 /**
  * Start our engines.
  */
-add_action( 'init', __NAMESPACE__ . '\add_bulk_action_items' );
+add_action( 'init', __NAMESPACE__ . '\manage_enabled_bulk_action_items' );
 
 /**
  * Add the bulk actions to an applicable dropdown.
  */
-function add_bulk_action_items() {
+function manage_enabled_bulk_action_items() {
 
 	// Get all the post types we suppor.
 	$enabled_types  = AdminConfig\get_enabled_post_types();
 
 	// Loop and add the filter.
 	foreach ( $enabled_types as $enabled_type ) {
-		add_filter("bulk_actions-edit-{$enabled_type}", __NAMESPACE__ . '\manage_bulk_actions', 30);
+		add_filter("bulk_actions-edit-{$enabled_type}", __NAMESPACE__ . '\add_enabled_bulk_action', 30);
 	}
 }
 
 /**
- * Remove bulk actions from the table views on workflow items.
+ * Add our option to the dropdown.
  *
  * @param  array $actions  Our existing array.
  *
  * @return array           The resulting array.
  */
-function manage_bulk_actions( $actions ) {
+function add_enabled_bulk_action( $actions ) {
 
 	// Don't show this for users who can't.
 	if ( ! current_user_can( AdminConfig\get_required_user_cap() ) ) {
@@ -52,8 +52,8 @@ function manage_bulk_actions( $actions ) {
 		return $actions;
 	}
 
-	// Add the author assignment item.
-	$actions['ccs-run-bulk'] = __( 'Add to Cold Storage', 'content-cold-storage' );
+	// Add our action.
+	$actions['ccs-run-bulk'] = __( 'Move to Cold Storage', 'content-cold-storage' );
 
 	// And return the resulting array.
 	return $actions;
