40abe4b942b4321f418d74e369d4b2e1610db665
Norcross <andrew.norcross@gmail.com>
2026-09-15 11:38:41 -0400

    adding revert

--- includes/admin/bulk-actions.php
+++ includes/admin/bulk-actions.php
@@ -15,6 +15,7 @@ use Norcross\ContentColdStorage\Admin\Config as AdminConfig;
  * Start our engines.
  */
 add_action( 'init', __NAMESPACE__ . '\manage_enabled_bulk_action_items' );
+add_filter( 'bulk_actions-edit-cold-storage', __NAMESPACE__ . '\add_revert_bulk_action', 30 );
 
 /**
  * Add the bulk actions to an applicable dropdown.
@@ -26,12 +27,12 @@ function manage_enabled_bulk_action_items() {
 
 	// Loop and add the filter.
 	foreach ( $enabled_types as $enabled_type ) {
-		add_filter("bulk_actions-edit-{$enabled_type}", __NAMESPACE__ . '\add_enabled_bulk_action', 30);
+		add_filter( "bulk_actions-edit-{$enabled_type}", __NAMESPACE__ . '\add_enabled_bulk_action', 30 );
 	}
 }
 
 /**
- * Add our option to the dropdown.
+ * Add our option to the dropdown for enabled types.
  *
  * @param  array $actions  Our existing array.
  *
@@ -54,6 +55,35 @@ function add_enabled_bulk_action( $actions ) {
 
 	// Add our action.
 	$actions['ccs-run-bulk'] = __( 'Move to Cold Storage', 'content-cold-storage' );
+
+	// And return the resulting array.
+	return $actions;
+}
+
+/**
+ * Add our option to the dropdown to revert.
+ *
+ * @param  array $actions  Our existing array.
+ *
+ * @return array           The resulting array.
+ */
+function add_revert_bulk_action( $actions ) {
+
+	// Don't show this for users who can't.
+	if ( ! current_user_can( AdminConfig\get_required_user_cap() ) ) {
+		return $actions;
+	}
+
+	// Grab the post status.
+	$ison_table = filter_input( INPUT_GET, 'post_status', FILTER_SANITIZE_SPECIAL_CHARS );
+
+	// Don't attempt to modify things in the trash.
+	if ( ! empty( $ison_table ) && 'trash' === $ison_table ) {
+		return $actions;
+	}
+
+	// Add our action.
+	$actions['ccs-revert-bulk'] = __( 'Restore to Original', 'content-cold-storage' );
 
 	// And return the resulting array.
 	return $actions;
