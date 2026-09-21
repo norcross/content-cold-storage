e7490fdaab2117b3dfb164bba8cac901cc26b1c0
Norcross <andrew.norcross@gmail.com>
2026-09-21 12:06:34 -0400

    allowing front-end viewing by logged in users

--- includes/structure/post-types.php
+++ includes/structure/post-types.php
@@ -8,10 +8,14 @@
 // Declare our namespace.
 namespace Norcross\ContentColdStorage\Structure\PostTypes;
 
+// Set our aliases.
+use Norcross\ContentColdStorage\Admin\Config as AdminConfig;
+
 /**
  * Start our engines.
  */
 add_action( 'init', __NAMESPACE__ . '\register_cold_storage_post_type' );
+add_action( 'template_redirect', __NAMESPACE__ . '\restrict_cold_storage_access' );
 
 /**
  * Set the cold storage post type.
@@ -52,7 +56,7 @@ function register_cold_storage_post_type() {
 		'show_in_nav_menus'   => false,
 		'can_export'          => true,
 		'exclude_from_search' => true,
-		'publicly_queryable'  => false,
+		'publicly_queryable'  => true,
 		'capability_type'     => 'post',
 		'capabilities'        => [
 			'create_posts' => false,
@@ -65,4 +69,29 @@ function register_cold_storage_post_type() {
 
 	// And register the post type.
 	register_post_type( 'cold-storage', $set_type_args );
+}
+
+/**
+ * Never allow front-end access to non-logged in users.
+ *
+ * @return void
+ */
+function restrict_cold_storage_access() {
+
+	// Only run this on the front-end, non-RSS feeds, non-logged in, and not REST stuff.
+	if ( is_admin() || is_feed() || is_user_logged_in() || wp_is_serving_rest_request() ) {
+		return;
+	}
+
+	// This only applies to singular cold storage items.
+	if ( ! is_singular( 'cold-storage' ) ) {
+		return;
+	}
+
+	// Set our default redirect link with a filter.
+	$setup_redirect = apply_filters( \Norcross\ContentColdStorage\ACTION_PREFIX . 'front_redirect_url', home_url( '/' ) );
+
+	// We met all the criteria, so redirect.
+	wp_safe_redirect( esc_url( $setup_redirect ), 302 );
+	exit();
 }
